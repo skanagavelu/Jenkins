a49bd9f1bb3df336f20f013b31f8eecdae33e06c
Kanagavelu Sugumar <skanagavelu@users.noreply.github.com>
2019-11-21 19:34:39 +0530

    Add files via upload

---
+++ test.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    stages {
+         stage('Validate Fail fast') {
+             failFast true
+             parallel {
+                  stage('stage A') {
+                    steps {
+                        echo 'stage A started'
+                        sleep 5
+                        script {
+                            error 'Test error'
+                        }
+                        echo "RESULT: ${currentBuild.result}"
+                        echo 'stage A Ended' //will not execute because of above sh return
+                    }
+                }
+                stage('stage B') {
+                    steps {
+                        echo 'stage B started'
+                        sleep 10
+                        echo 'stage B wakeup'
+                        echo "RESULT: ${currentBuild.result}"
+                        echo 'stage B Ended' //will not execute because of above stage fail
+                    }
+                }
+                 stage('stage C') {
+                    steps {
+                        echo 'stage C started'
+                        echo 'stage C Ended' //will not execute because of above stage fail
+                    }
+                }
+             }
+         }
+         stage('final stage sequential') {
+             steps {
+                 script {
+                     echo "The complete run!"
+                 }
+             }
+         }
+     }
+}
