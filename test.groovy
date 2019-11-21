e0a5bc7c01017389f9d4b30bc7dde7a5c8e22407
skanagavelu <skanagavelu@gmail.com>
2019-11-21 22:24:17 +0530

    .

--- test.groovy
+++ test.groovy
@@ -1,15 +1,17 @@
-node{
-checkout scm
-  // To know files are checked out or not
-    sh '''
-        ls -lhrt
-    '''
-
-def rootDir = pwd()
-    println("Current Directory: " + rootDir)
-
-
-def c = load "${rootDir}/common.groovy"
-c.mycommoncode()
-println "Hello World!"
+pipeline {
+    agent any
+    stages {
+        stage('final stage sequential') {
+            steps {
+              script {
+                  checkout scm
+                  def rootDir = pwd()
+                  println("Current Directory: " + rootDir)
+                  def c = load "${rootDir}/common.groovy"
+                  c.mycommoncode()
+                  println "Hello World!"
+              }
+            }
+        }
+     }
 }
